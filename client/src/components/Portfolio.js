c18dce69a3bbcbc10eb3743005e078eb83736a00
Rocky Primm <primmrock@gmail.com>
2024-01-11 12:05:06 -0500

    added real project

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -1,23 +1,37 @@
-import React from 'react'
+import React from "react";
 
-import Project from './Project'
+import Project from "./Project";
 
 function Portfolio() {
   return (
-    <div className='container' id='portfolio' style={{backgroundColor: '#ededed'}}>
-        <div className='portfolio-content align'>
-          <div className='title'>
-            <h1>Portfolio</h1>
-          </div>
-          <div className='portfolio-main'>
-            <Project img={process.env.PUBLIC_URL + '/assets/dogs-in-the-forest.webp'} title={'Fake Store'} languages={['React', 'Node.js']} />
-            <Project img={process.env.PUBLIC_URL + '/assets/dogs-in-the-forest.webp'} title={'Fake Store'} languages={['React', 'Node.js']} />
-            <Project img={process.env.PUBLIC_URL + '/assets/dogs-in-the-forest.webp'} title={'Fake Store'} languages={['React', 'Node.js']} />
-            <Project img={process.env.PUBLIC_URL + '/assets/dogs-in-the-forest.webp'} title={'Fake Store'} languages={['React', 'Node.js']} />
-          </div>
+    <div
+      className="container"
+      id="portfolio"
+      style={{ backgroundColor: "#ededed" }}
+    >
+      <div className="portfolio-content align">
+        <div className="title">
+          <h1>Portfolio</h1>
+        </div>
+        <div className="portfolio-main">
+          <Project
+            img={process.env.PUBLIC_URL + "/assets/PokemonIMG.png"}
+            title={"Pokemon Catelogue"}
+            languages={["React"]}
+            link={"https://rprimm.github.io/pokemon/"}
+            github={"https://github.com/rprimm/pokemon"}
+          />
+          <Project
+            img={process.env.PUBLIC_URL + "/assets/Winows95.png"}
+            title={"Windows95 Emulator"}
+            languages={["HTML", "CSS", "JavaScript"]}
+            link={'https://rprimm.github.io/windows95'}
+            github={'https://github.com/rprimm/windows95'}
+          />
         </div>
       </div>
-  )
+    </div>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -1,29 +1,36 @@
-import React from 'react'
-import { SiGithub } from 'react-icons/si';
-import { BsArrowRightSquareFill } from 'react-icons/bs'
+import React from "react";
+import { SiGithub } from "react-icons/si";
+import { BsArrowRightSquareFill } from "react-icons/bs";
 
-function Project({ img, title, languages }) {
-
+function Project({ img, title, languages, link, github }) {
   return (
-    <div className='project'>
-        <img src={img} alt='Project image'/>
-        <div className='project-info'>
-            <div className='project-top'>
-                <h3 className='font-m' style={{margin: 0}}>Fake Store</h3>
-                <div className='project-top-buttons'>
-                <button><SiGithub /></button>
-                <button><BsArrowRightSquareFill /></button>
-                </div>     
-            </div>
-            <div className='project-footer'>
-                <div className='project-footer-languages font-s'>
-                <div className='p-react font-s'>React</div>
-                <div className='p-node'>Node.js</div>
-            </div>
+    <div className="project">
+      <img src={img} alt="Project image" />
+      <div className="project-info">
+        <div className="project-top">
+          <h3 className="font-m" style={{ margin: 0 }}>
+            {title}
+          </h3>
+          <div className="project-top-buttons"></div>
         </div>
-        </div> 
+        <div className="project-footer">
+          <div className="project-footer-languages font-s">
+            {languages.map((language) => {
+              return <div className="font-s">{language}</div>;
+            })}
+          </div>
+          <div className="project-footer-buttons">
+            <button className="project-button">
+              <a href={github}><SiGithub /></a>
+            </button>
+            <button className="project-button">
+              <a href={link}><BsArrowRightSquareFill /></a>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Project
+export default Project;
